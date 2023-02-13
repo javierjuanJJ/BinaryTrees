58c3250f566b77db7ee3e966a07e7e54d44a7c25
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-13 10:04:11 +0100

    Commit 2 part1

--- part1.js
+++ part1.js
@@ -1,1 +1,22 @@
 console.log("hellow");
+class Node {
+    constructor(props) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+}
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.right = d;
+b.left = e;
+c.right = f;
