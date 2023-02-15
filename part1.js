7082e3a1ac7569a4ecd4998fdc514d2a28e02ecc
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-15 10:05:24 +0100

    Commit 2 part1

--- part1.js
+++ part1.js
@@ -26,7 +26,24 @@ const treeSum = (root) => {
     if (root == null){
         return 0;
     }
-    return root.val + treeSum(root.left) + treeSum(root.right);
+    let totalSum = 0;
+    const  queue =[ root ];
+
+    while (queue.length > 0){
+        const current = queue.shift();
+        totalSum += current.val;
+
+        if (current.left != null){
+            queue.push(current.left);
+        }
+
+        if (current.right != null){
+            queue.push(current.right);
+        }
+    }
+
+
+    return totalSum;
 }
 console.log(treeSum(a));
 
